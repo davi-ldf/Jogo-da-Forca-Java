c89e12ef89884feebffacd9919c2e31f36a5dbaf
Davi Lisboa <davifranca135@gmail.com>
2026-02-03 23:08:45 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import  java.util.Random;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
